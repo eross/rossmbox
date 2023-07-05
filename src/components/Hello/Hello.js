1911eb1a2c143448a9480713073b959d9ab0ac3a
Eric Ross <epr@rossmbox.com>
2023-07-05 09:36:52 -0700

    Add iframe

---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Hello.css';
+const Hello = () => {
+    return (
+        <iframe src="https://fredaccount.stlouisfed.org/dashboard/98976" title="FRED data"></iframe>
+    )
+}
+
+export default Hello;
